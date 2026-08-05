b2602cca394675c1ed7e16f9c7e70596f80d3138
mrohansanjay-hub <mrohansanjay@gmail.com>
2026-08-05 16:57:29 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,30 +2,31 @@
 
 set -e
 
-SERVER=$1
+echo "======================================="
+echo "Deploying Application"
+echo "======================================="
 
-echo "Deploying to ${SERVER}"
-
-ssh -o StrictHostKeyChecking=no ubuntu@${SERVER} << EOF
+ssh -o StrictHostKeyChecking=no ubuntu@${APP_SERVER} <<EOF
 
 set -e
 
-cd /opt/business-platform
+cd ${APP_PATH}
 
-sed -i "s/^IMAGE_TAG=.*/IMAGE_TAG=${BUILD_NUMBER}/" .env
+sed -i "s/^IMAGE_TAG=.*/IMAGE_TAG=${IMAGE_TAG}/" .env
 
-aws ecr get-login-password --region eu-north-1 | \
+aws ecr get-login-password --region ${AWS_REGION} | \
 docker login \
 --username AWS \
---password-stdin \
-032844082845.dkr.ecr.eu-north-1.amazonaws.com
+--password-stdin ${ECR}
 
 docker compose -f compose.prod.yaml pull
 
 docker compose -f compose.prod.yaml up -d --remove-orphans
 
-docker image prune -af
+docker image prune -f
 
 docker ps
 
 EOF
+
+echo "Deployment Finished Successfully"
