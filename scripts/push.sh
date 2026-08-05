995a86e5b1edd1acf87e0a9cb274e46429d88c91
mrohansanjay-hub <mrohansanjay@gmail.com>
2026-08-05 19:20:16 +0530

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-echo "======================================="
-echo "Pushing Images to ECR"
-echo "======================================="
+echo "====================================="
+echo "Pushing Docker Images to ECR"
+echo "====================================="
 
-docker push ${ECR}/erp-backend:${IMAGE_TAG}
-docker push ${ECR}/erp-frontend:${IMAGE_TAG}
-docker push ${ECR}/pos-backend:${IMAGE_TAG}
-docker push ${ECR}/pos-frontend:${IMAGE_TAG}
+docker compose -f compose.build.yaml push
 
-echo
-echo "======================================="
-echo "Push Completed Successfully"
-echo "======================================="
+echo "====================================="
+echo "Push Complete"
+echo "====================================="
